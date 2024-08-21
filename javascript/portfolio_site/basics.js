d2dbd5d10fcc41312f92000e04a7c4b3fcdeadf7
Eamann Miled <130555562+Eamanmiled@users.noreply.github.com>
2024-08-21 22:57:46 +0100

    doesnt work but want to do 75 blind

--- javascript/portfolio_site/basics.js
+++ javascript/portfolio_site/basics.js
@@ -2,10 +2,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const box = document.getElementById('confirm_box');
     
     box.addEventListener('click', () => {
-        alert('Unsubscribed!');
+        alert('Unsubscribed from annoying spam!');
     });
 });
 
 document.getElementById('switchButton').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
 });
+
+function showContent(pageId) {
+    var contents = document.querySelectorAll('.page-content');
+    contents.forEach(function(content) {
+        content.style.display = 'none';
+    });
+    var selectedContent = document.getElementById(pageId);
+    selectedContent.style.display = 'block';
+}
+showContent('home');
